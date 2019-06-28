cbf1e62a6315a8ab7f73e2b737d733d11d795e3d
Victoy <anavictoy@yahoo.com.br>
2019-06-28 20:29:59 -0300

    Falta dois bugs e subir

--- static/js/modules/recuperarDados.js
+++ static/js/modules/recuperarDados.js
@@ -6,19 +6,15 @@ export function setPageIcon(url){
     link.rel = 'shortcut icon';
     link.href = url
     document.getElementsByTagName('head')[0].appendChild(link);  
-    //console.log(document.getElementsByTagName('head')[0])  
-    //console.log(link)
 }
 
 export function recuperarDadosPainel(){
 	let url= window.location.href.split("/")
 	let id=url[url.length-1]
 	var data=new Object()
-	
 	data.id=id
 	let bd;
 	let cena;
-	//console.log(data)
 	url='/pegarMundo/'
 	 $.ajax({
         type: 'POST',
@@ -27,10 +23,9 @@ export function recuperarDadosPainel(){
         async: false
 
     }).done(function(msg){
-    	console.log("MUNDO RETORNADO PELO NODE")
-       // console.log(msg); // log here 
         bd=msg;
     });
+
     url='/pegarCena/'
 	 $.ajax({
         type: 'POST',
@@ -39,25 +34,18 @@ export function recuperarDadosPainel(){
         async: false
 
     }).done(function(msg){
-    	console.log("CENA RETORNADO PELO NODE")
-        //console.log(msg); // log here 
         cena=msg;
     });
-    console.log(bd)
-    console.log(cena)
+
 	let Dados = new Object();
 	Dados.icon = bd["icon"];
-	//console.log(Dados.icon)
-
 	Dados.num_imagens = bd["num_imagens"];
 	Dados.num_textos = bd["num_textos"];
 	Dados.num_videos = bd["num_videos"];
 	Dados.num_musicas = bd["num_musicas"];
 	Dados.num_galerias = bd["num_galerias"];
-	//Dados.galerias=null
 	Dados.galerias = JSON.parse(JSON.stringify(bd["galerias"]));	
-	//Dados.icon = localStorage.getItem("icon");
-	//console.log(Dados)
+
 	if (Dados.num_imagens == null || Dados.num_imagens == "undefined") { 
 		Dados.num_imagens = 0; 
 	}
@@ -80,25 +68,23 @@ export function recuperarDadosPainel(){
 	Dados.backgroundTexture = cena["backgroundTexture"];	
 	Dados.backgroundColor = cena["backgroundColor"];
 
-	if(Dados.backgroundTexture != null && Dados.backgroundTexture != "null" && Dados.backgroundTexture != "undefined"){
+	if(Dados.backgroundTexture != '' && Dados.backgroundTexture != null && Dados.backgroundTexture != "null" && Dados.backgroundTexture != "undefined"){
 		let background = document.querySelector("#fundo");
 		background.style.backgroundImage = "url(\"../static/Sprites/floor_textures/" + Dados.backgroundTexture + "\")";
-		//console.log("url(\"../static/Sprites/floor_textures/" + Dados.backgroundTexture + "\")")
 		background.style.backgroundColor = "none";
 	}
 	
-	if(Dados.backgroundColor != null && Dados.backgroundColor != "null" && Dados.backgroundColor != "undefined"){
+	if(Dados.backgroundColor != '' && Dados.backgroundColor != null && Dados.backgroundColor != "null" && Dados.backgroundColor != "undefined"){
 		let background = document.querySelector("#fundo");
 		background.style.backgroundImage = "none";
 		background.style.backgroundColor = Dados.backgroundColor;
 	}
 
 	Dados.floorTexture = cena["floorTexture"];
-	if(Dados.floorTexture != null && Dados.floorTexture != "null" && Dados.floorTexture != "undefined"){	
+	if(Dados.floorTexture != '' && Dados.floorTexture != null && Dados.floorTexture != "null" && Dados.floorTexture != "undefined"){	
 		let chao = document.getElementById("chao");
 		chao.style.backgroundImage = "url(\"../static/Sprites/floor_textures/" + Dados.floorTexture + "\")";
 	}
-	//console.log(Dados)
 	let mundo = document.querySelector("#mundo");
 	for (let i = 1; i <= Number(Dados.num_imagens); i++) {
 		let objeto = document.createElement("img");
@@ -106,20 +92,12 @@ export function recuperarDadosPainel(){
 		objeto.outerHTML = bd["imagem_"+i+"_outerHTML"];
 		objeto.innerHTML = bd["imagem_"+i+"_innerHTML"];
 		objeto.addEventListener('click', criarPropriedades);
-		/*console.log("IMG")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)*/
 	}
 	for (let i = 1; i <= Number(Dados.num_textos); i++) {
 		let objeto = document.createElement("textarea");
 		mundo.appendChild(objeto);
 		objeto.outerHTML = bd["texto_"+i+"_outerHTML"];
 		objeto.innerHTML = bd["texto_"+i+"_innerHTML"];
-		/*console.log("TEXTO")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)*/
 		objeto.addEventListener('click', criarPropriedades);
 	}
 	for (let i = 1; i <= Number(Dados.num_videos); i++) {
@@ -127,10 +105,6 @@ export function recuperarDadosPainel(){
 		mundo.appendChild(objeto);
 		objeto.outerHTML = bd["video_"+i+"_outerHTML"];
 		objeto.innerHTML = bd["video_"+i+"_innerHTML"];
-		/*console.log("VIDEO")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)*/
 		objeto.addEventListener('click', criarPropriedades);
 	}
 	for (let i = 1; i <= Number(Dados.num_musicas); i++) {
@@ -138,10 +112,6 @@ export function recuperarDadosPainel(){
 		mundo.appendChild(objeto);
 		objeto.outerHTML = bd["musica_"+i+"_outerHTML"];
 		objeto.innerHTML = bd["musica_"+i+"_innerHTML"];
-		/*console.log("MUSICA")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)*/
 		objeto.addEventListener('click', criarPropriedades);
 	}
 	for (let i = 1; i <= Number(Dados.num_galerias); i++) {
@@ -149,10 +119,6 @@ export function recuperarDadosPainel(){
 		mundo.appendChild(objeto);
 		objeto.outerHTML = bd["galeria_"+i+"_outerHTML"];
 		objeto.innerHTML = bd["galeria_"+i+"_innerHTML"];
-		/*console.log("MUSICA")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)*/
 		objeto.addEventListener('click', criarPropriedades);
 	}
 
@@ -172,133 +138,6 @@ export function recuperarDadosPainel(){
 			setPageIcon(Dados.icon);
 		}
 	}
-	/*let Dados = new Object();
-	Dados.icon = localStorage.getItem("icon");
-
-
-	Dados.num_imagens = localStorage.getItem("num_imagens");
-	Dados.num_textos = localStorage.getItem("num_textos");
-	Dados.num_videos = localStorage.getItem("num_videos");
-	Dados.num_musicas = localStorage.getItem("num_musicas");
-	Dados.num_galerias = localStorage.getItem("num_galerias");
-	Dados.galerias=null
-//Dados.galerias = JSON.parse(window.localStorage.getItem("galerias"));	
-	Dados.icon = localStorage.getItem("icon");
-
-	if (Dados.num_imagens == null || Dados.num_imagens == "undefined") { 
-		Dados.num_imagens = 0; 
-	}
-	if (Dados.num_textos == null || Dados.num_textos == "undefined") { 
-		Dados.num_textos = 0; 
-	}
-	if (Dados.num_videos == null || Dados.num_videos == "undefined") { 
-		Dados.num_videos = 0; 
-	}
-	if (Dados.num_musicas == null || Dados.num_musicas == "undefined") { 
-		Dados.num_musicas = 0; 
-	}
-	if (Dados.num_galerias == null || Dados.num_galerias == "undefined") { 
-		Dados.num_galerias = 0; 
-	}
-	if (Dados.galerias == null || Dados.galerias == "undefined") { 
-		Dados.galerias = {}; 
-	}
-
-	Dados.backgroundTexture = localStorage.getItem("backgroundTexture");	
-	Dados.backgroundColor = localStorage.getItem("backgroundColor");
-
-	if(Dados.backgroundTexture != null && Dados.backgroundTexture != "null" && Dados.backgroundTexture != "undefined"){
-		let background = document.querySelector("#fundo");
-		background.style.backgroundImage = "url(\"../static/Sprites/floor_textures/" + Dados.backgroundTexture + "\")";
-		background.style.backgroundColor = "none";
-	}
-	
-	if(Dados.backgroundColor != null && Dados.backgroundColor != "null" && Dados.backgroundColor != "undefined"){
-		let background = document.querySelector("#fundo");
-		background.style.backgroundImage = "none";
-		background.style.backgroundColor = Dados.backgroundColor;
-	}
-
-	Dados.floorTexture = localStorage.getItem("floorTexture");
-	if(Dados.floorTexture != null && Dados.floorTexture != "null" && Dados.floorTexture != "undefined"){	
-		let chao = document.getElementById("chao");
-		chao.style.backgroundImage = "url(\"../static/Sprites/floor_textures/" + Dados.floorTexture + "\")";
-	}
-
-	let mundo = document.querySelector("#mundo");
-	for (let i = 1; i <= Number(Dados.num_imagens); i++) {
-		let objeto = document.createElement("img");
-		mundo.appendChild(objeto);
-		objeto.outerHTML = localStorage.getItem("imagem-"+i+"_outerHTML");
-		objeto.innerHTML = localStorage.getItem("imagem-"+i+"_innerHTML");
-		objeto.addEventListener('click', criarPropriedades);
-		console.log("IMG")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)
-	}
-	for (let i = 1; i <= Number(Dados.num_textos); i++) {
-		let objeto = document.createElement("textarea");
-		mundo.appendChild(objeto);
-		objeto.outerHTML = localStorage.getItem("texto-"+i+"_outerHTML");
-		objeto.innerHTML = localStorage.getItem("texto-"+i+"_innerHTML");
-		console.log("TEXTO")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)
-		objeto.addEventListener('click', criarPropriedades);
-	}
-	for (let i = 1; i <= Number(Dados.num_videos); i++) {
-		let objeto = document.createElement("video");
-		mundo.appendChild(objeto);
-		objeto.outerHTML = localStorage.getItem("video-"+i+"_outerHTML");
-		objeto.innerHTML = localStorage.getItem("video-"+i+"_innerHTML");
-		console.log("VIDEO")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)
-		objeto.addEventListener('click', criarPropriedades);
-	}
-	for (let i = 1; i <= Number(Dados.num_musicas); i++) {
-		let objeto = document.createElement("audio");
-		mundo.appendChild(objeto);
-		objeto.outerHTML = localStorage.getItem("musica-"+i+"_outerHTML");
-		objeto.innerHTML = localStorage.getItem("musica-"+i+"_innerHTML");
-		console.log("MUSICA")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)
-		objeto.addEventListener('click', criarPropriedades);
-	}
-	for (let i = 1; i <= Number(Dados.num_galerias); i++) {
-		let objeto = document.createElement("div");
-		mundo.appendChild(objeto);
-		objeto.outerHTML = localStorage.getItem("galeria-"+i+"_outerHTML");
-		objeto.innerHTML = localStorage.getItem("galeria-"+i+"_innerHTML");
-		console.log("MUSICA")
-		console.log(objeto)
-		console.log("MUNDO")
-		console.log(mundo)
-		objeto.addEventListener('click', criarPropriedades);
-	}
-
-	Dados.myPage = localStorage.getItem("myPage");
-	if (Dados.myPage == null || Dados.myPage == "undefined") { 
-		Dados.myPage = true; 		
-	}
-	else if (Dados.myPage == "true"){
-		Dados.myPage = true; 
-	}
-	else if (Dados.myPage == "false"){
-		Dados.myPage = false; 
-	}
-
-	if(Dados.myPage){
-		if(Dados.icon != null && Dados.icon != "null" && Dados.icon != "undefined"){
-			setPageIcon(Dados.icon);
-		}
-	}*/
-	console.log(Dados)
 	return Dados;
 }
 
@@ -306,11 +145,11 @@ export function recuperarDadosCena(){
 	let Dados = new Object();
 	let url= window.location.href.split("/")
 	let idMundo=url[url.length-1]
-	console.log(idMundo)
-	var data=new Object()
+	let data=new Object()
 	data.id=idMundo
 	let CorAlienMundo;
-
+	let nomeAlienMundo,nomeAlienUsuario="VISITANTE";
+	
 	url='/pegarcormundo/'
 	$.ajax({
         type: 'POST',
@@ -319,26 +158,18 @@ export function recuperarDadosCena(){
         async: false
 
     }).done(function(msg){
-    	console.log("COR DO ALIEN DO MUNDO")
-        CorAlienMundo=msg;
-        console.log(CorAlienMundo)
+        CorAlienMundo=msg.alien;
+        nomeAlienMundo=msg.usuario;
     });
     
-    
-
-
 	let cookie=document.cookie.split(";")
-	console.log(cookie)
 	let idUsuario=-1;
 	let CorAlienUsuario;
 	let user;
 	for (var i =cookie.length - 1; i >= 0; i--) {
-		//console.log(cookie[i])
-		//console.log(cookie[i].indexOf("usuario"))
 		if(cookie[i].indexOf("usuario")!=-1){
 			user=cookie[i].split("=")
 			idUsuario=user[1]
-			console.log("USER ATUAL: "+idUsuario)
 		}
 	}
 
@@ -350,38 +181,41 @@ export function recuperarDadosCena(){
         url:url, 
         data:data,
         async: false
-
     	}).done(function(msg){
-    		console.log("COR DO ALIEN DO USUARIO")
-        	CorAlienUsuario=msg;
-        	console.log(CorAlienUsuario)
+        	CorAlienUsuario=msg.alien;
+        	nomeAlienUsuario=msg.usuario;
     	});
-    	//PEGAR ESTADO DA VAQUINHA, 
-    	
 	}
-
+	let alienMundo=document.getElementById("userCharacter")
+	let alienVisitante=document.getElementById("visitorCharacter")
+	
 	Dados.userColor=CorAlienUsuario;
 	let visitanteCadatrado=false;
 	if(idUsuario==-1||idUsuario==undefined){
 		Dados.userColor=CorAlienMundo;
 		Dados.visitorColor = "green";
 		Dados.myPage=false;
+		alienVisitante.title="VISITANTE";
+		alienMundo.title=nomeAlienMundo;
 		data.visitante=1
 	}
 	else if(idUsuario==idMundo){
 		Dados.visitorColor = null;
 		Dados.myPage=true;
-		data.visitante=0
+		alienVisitante.title=""
+		data.visitante=0;
+		alienMundo.title=nomeAlienMundo;
 	}else{
 		Dados.userColor=CorAlienMundo;
 		Dados.visitorColor=CorAlienUsuario;
 		Dados.myPage=false;
+		alienVisitante.title=nomeAlienUsuario;
+		alienMundo.title=nomeAlienMundo;
 		data.visitante=1
 	}
 	let visitas=0;
 	let estado="";
 	data.id=idMundo
-		//data.visitante=Dados.myPage
 	url='/visita'
 	$.ajax({
         type: 'POST',
@@ -399,6 +233,5 @@ export function recuperarDadosCena(){
 	vis.title="VISITAS="+visitas
 	est.title="EU ESTOU "+estado
 	Dados.visitas=visitas
-	console.log(Dados)
 	return Dados;
 }
--- static/js/modules/salvarDados.js
+++ static/js/modules/salvarDados.js
@@ -1,5 +1,4 @@
 export function salvarDadosPortifolio(Dados){
-	console.log(document.getElementById("mundo"))
 	var data=new Object()
 	let url= window.location.href.split("/")
 	data.id=url[url.length-1]
@@ -13,40 +12,35 @@ export function salvarDadosPortifolio(Dados){
 		if(objeto[i].id.startsWith("texto")) objeto[i].innerHTML = objeto[i].value;
 		if(objeto[i].id.startsWith("video")) objeto[i].firstChild.style.pointerEvents = "none";
 		if(objeto[i].id.startsWith("musica")) objeto[i].firstChild.style.pointerEvents = "none";
-
 		objeto[i].classList.remove("resize-drag")
-
-		localStorage.setItem(objeto[i].id+"_outerHTML",objeto[i].outerHTML);
 		var nome = objeto[i].id+"_outerHTML"
 		nome = nome.replace("-","_")
 		data[nome]=objeto[i].outerHTML
-		localStorage.setItem(objeto[i].id+"_innerHTML",objeto[i].innerHTML);
 		nome = objeto[i].id+"_innerHTML"
 		nome = nome.replace("-","_")
 		data[nome]=objeto[i].innerHTML
     }
-    localStorage.setItem("galerias",JSON.stringify(Dados.galerias));
 	data.galerias=JSON.stringify(Dados.galerias)
-	localStorage.setItem("num_textos",Dados.num_textos);
 	data.num_textos=Dados.num_textos
-	localStorage.setItem("num_imagens",Dados.num_imagens);
 	data.num_imagens=Dados.num_imagens
-    localStorage.setItem("num_videos",Dados.num_videos);
     data.num_videos=Dados.num_videos
-    localStorage.setItem("num_musicas",Dados.num_musicas);
     data.num_musicas=Dados.num_musicas
-    localStorage.setItem("num_galerias",Dados.num_galerias);
     data.num_galerias=Dados.num_galerias
-    localStorage.setItem("icon",Dados.icon);
     data.icon=Dados.icon
-//    data.visitas=0
 
     $.ajax({
         type: 'POST',
         url: '/salvarmundo', 
         data: data
     }).done(function(msg){
-        console.log("Mundo Salvo")
+    	//Isso estava funcionando mas parou do nada
+    	//mas que devia funciona devia
+    	let aviso = document.getElementById("aviso")
+    	aviso.innerText="Salvo!"
+    	let a = setTimeout(()=> {
+			let aviso = document.getElementById("aviso")
+			aviso.innerText=""
+		}, 1000)
     });
 }
 
@@ -54,20 +48,23 @@ export function salvarDadosCena(Dados){
 	var data=new Object()
 	let url= window.location.href.split("/")
 	data.id=url[url.length-1]
-	//localStorage.setItem("backgroundTexture", Dados.backgroundTexture);
 	data.backgroundTexture=Dados.backgroundTexture
-	//localStorage.setItem("backgroundColor", Dados.backgroundColor);
 	data.backgroundColor=Dados.backgroundColor
-	//localStorage.setItem("floorTexture", Dados.floorTexture);
 	data.floorTexture=Dados.floorTexture
-	///localStorage.setItem("userColor", Dados.userColor);
 	data.alien=Dados.userColor
 	$.ajax({
         type: 'POST',
-        url: '/salvarcena', // This one is missing here
+        url: '/salvarcena', 
         data: data
     }).done(function(msg){
-        console.log("Cena Salva"); // log here 
+    	//Isso estava funcionando mas parou do nada
+    	//mas que devia funciona devia
+    	let aviso = document.getElementById("aviso")
+    	aviso.innerText="Salvo!"
+    	let a = setTimeout(()=> {
+			let aviso = document.getElementById("aviso")
+			aviso.innerText=""
+		}, 1000)
     });
 }
 
